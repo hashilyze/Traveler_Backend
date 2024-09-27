d4ea032f2280724056ea33bf0877b5174e11e0aa
Junsu Bae <jsbae1023@gmail.com>
2024-09-27 16:30:13 +0900

    feature: Describe restaurants info

--- server.py
+++ server.py
@@ -22,16 +22,39 @@ class User(db.Model):
         self.user_code = user_code
 
 
+class Business(db.Model):
+    __table_name__ = "business"
+    business_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    business_code = db.Column(db.String(22), unique=True, nullable=False)
+    longitude = db.Column(db.Double)
+    latitude = db.Column(db.Double)
+
+    def __init__(self, business_id, business_code, longitude, latitude) :
+        self.business_id = business_id
+        self.business_code = business_code
+        self.longitude = longitude
+        self.latitude = latitude
+
+
 @app.get('/recommend/<int:id>')
 def recommend(id):
     restaurants = recommend_restaurants(id)
-    result = {
-        'user': {
+    result = { }
+    result['user'] = {
             'id': id,
             'name': db.session.query(User.name).filter(User.user_id == id).scalar()
-        },
-        'restaurants': restaurants.tolist()
+        }
+
+    result['restaurants'] = {
+        'count': len(restaurants.tolist()),
+        'list': []
     }
+    for restaurant_id in restaurants.tolist():
+        result['restaurants']['list'].append({
+            'id': restaurant_id,
+            'longitude': db.session.query(Business.longitude).filter(Business.business_id == restaurant_id).scalar(),
+            'latitude': db.session.query(Business.latitude).filter(Business.business_id == restaurant_id).scalar()
+        })
 
     return result
 
