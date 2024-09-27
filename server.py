5d3619afb948497005973110bda5d8f45b680a1e
Junsu Bae <jsbae1023@gmail.com>
2024-09-27 16:13:28 +0900

    feature: Add db connection

--- server.py
+++ server.py
@@ -1,17 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from BPR.BPR import recommend_restaurants
+from secret.config import *
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy()
+db.init_app(app)
 
-
-from BPR.BPR import recommend_restaurants
 
 @app.get('/recommend/<int:id>')
 def recommend(id):
-    print(id)
-    result = recommend_restaurants(id)
-    return {
-        'user': id,
-        'restuarnts': result.tolist()
+    restaurants = recommend_restaurants(id)
+    result = {
+        'id': id,
+        'restaurants': restaurants.tolist()
     }
+
+    return result
+
 
 app.run(debug=True)
