cec417eb0946fab9d192568f52fde300da563fc7
Junsu Bae <jsbae1023@gmail.com>
2024-09-27 16:18:39 +0900

    feature: Describe user Info

--- server.py
+++ server.py
@@ -10,11 +10,26 @@ db = SQLAlchemy()
 db.init_app(app)
 
 
+class User(db.Model):
+    __table_name__ = "user"
+    user_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_code = db.Column(db.String(22), unique=True, nullable=False)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
+    def __init__(self, user_id, name, user_code) :
+        self.user_id = user_id
+        self.name = name
+        self.user_code = user_code
+
+
 @app.get('/recommend/<int:id>')
 def recommend(id):
     restaurants = recommend_restaurants(id)
     result = {
-        'id': id,
+        'user': {
+            'id': id,
+            'name': db.session.query(User.name).filter(User.user_id == id).scalar()
+        },
         'restaurants': restaurants.tolist()
     }
 
