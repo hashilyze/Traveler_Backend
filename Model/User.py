9d873bdcf2980e8f35b81844b966c6dd31fbf02b
Junsu Bae <jsbae1023@gmail.com>
2024-09-28 09:52:49 +0900

    refactor: Model 정의 수정

--- Model/User.py
+++ Model/User.py
@@ -6,7 +6,7 @@ class User(db.Model):
     __table_name__ = "user"
     user_id = db.Column(db.Integer, primary_key=True, nullable=False)
     user_code = db.Column(db.String(22), unique=True, nullable=False)
-    name = db.Column(db.String(50), unique=True, nullable=False)
+    name = db.Column(db.String(80))
 
     def __init__(self, user_id, user_code, name):
         self.user_id = user_id
